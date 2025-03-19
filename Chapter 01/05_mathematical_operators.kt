3f70650daeed79baa0106851a429e7733fa78011
Dostonjon-98 <mahmanazarovdoston98@gmail.com>
2025-03-19 12:29:33 +0500

    Added mathematical operators file

---
+++ Chapter 01/05_mathematical_operators.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val a = 10
+    val b = 2
+
+    val sum = a + b // 10+2=12
+    val difference = a - b // 10-2=8
+    val division = a / b // 10/2=5
+    val multiplication = a * b // 10*2=20
+    val modulus = a % b // 10%2=0
+
+    println("sum = $sum") // sum = 12
+    println("difference = $difference") // difference = 8
+    println("division = $division") // division = 5
+    println("multiplication = $multiplication") // multiplication = 20
+    println("modulus = $modulus") // modulus = 0
+}
