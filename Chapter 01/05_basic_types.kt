7b488b827e41b700fbac3a7549e6f1675cbcc62c
Dostonjon-98 <mahmanazarovdoston98@gmail.com>
2025-03-24 16:24:04 +0500

    Added basic types file

---
+++ Chapter 01/05_basic_types.kt
@@ -0,0 +1,29 @@
+fun main() {
+    val binary = 0b01
+
+    val oneByt: Byte = 1        // Byte
+
+    val oneShort: Short = 1     // Short
+
+    val oneInt = 1              // Int
+    val twoInt: Int = 2         // Int
+
+    val oneLong = 1L            // Long
+    val twoLong: Long = 2       // Long
+
+    val oneDouble: Double = 1.0 // Double
+    val pi = 3.14               // Double
+
+    val oneFloat = 1f           // Float
+
+    println(binary)     // 1
+    println(oneByt)     // 1
+    println(oneShort)   // 1
+    println(oneInt)     // 1
+    println(twoInt)     // 2
+    println(oneLong)    // 1
+    println(twoLong)    // 2
+    println(oneDouble)  // 1.0
+    println(pi)         // 3.14
+    println(oneFloat)   // 1.0
+}
